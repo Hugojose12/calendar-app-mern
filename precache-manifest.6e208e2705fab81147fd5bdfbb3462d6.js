9b677ebefab5ff460945097f895101fb6b34ff60
Hugo Rivero <hugo200440@gmail.com>
2020-11-25 23:26:44 -0400

    Updates

---
+++ precache-manifest.6e208e2705fab81147fd5bdfbb3462d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565841bc85ab924b9bd2737daf5fa5bd",
+    "url": "/calendar-app-mern/index.html"
+  },
+  {
+    "revision": "ef1cd026487a2620c2d2",
+    "url": "/calendar-app-mern/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "e8936f5237b2794b48c0",
+    "url": "/calendar-app-mern/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "ef1cd026487a2620c2d2",
+    "url": "/calendar-app-mern/static/js/2.fa81b15d.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/calendar-app-mern/static/js/2.fa81b15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8936f5237b2794b48c0",
+    "url": "/calendar-app-mern/static/js/main.9c78a392.chunk.js"
+  },
+  {
+    "revision": "f21e9bb667c30fcadd6e",
+    "url": "/calendar-app-mern/static/js/runtime-main.a9742baf.js"
+  }
+]);
